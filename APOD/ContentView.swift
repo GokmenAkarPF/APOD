7915ab9e11b284e50c52f06b2275c3a59854b2f8
GokmenAkarPF <gokmen@propertyfinder.ae>
2023-09-05 16:38:09 +0300

    Add pagination for home view

--- APOD/ContentView.swift
+++ APOD/ContentView.swift
@@ -11,10 +11,26 @@ import SDWebImageSwiftUI
 class HomeViewModel: ObservableObject {
     @Published var models: [APOD] = []
 
-    func getPhotos() async {
+    var upperDate: Date
+    var lowerDate: Date
+    var currentDate = Date()
+
+    init() {
+        upperDate = currentDate
+        lowerDate = Calendar.current.date(byAdding: .day, value: -10, to: currentDate)!
+    }
+
+    @MainActor func getPhotos() async {
+
+        let upperBoundString = upperDate.formatted(.iso8601).description.split(separator: "T").first!.description
+        let lowerBoundString = lowerDate.formatted(.iso8601).description.split(separator: "T").first!.description
+
         do {
-            let (data, _) = try await URLSession.shared.data(from: URL(string: "https://api.nasa.gov/planetary/apod?api_key=WBd6KIwrJohInTFEi1XSZA7ERws6opS3KLm2XhSH&count=5")!)
+            let (data, _) = try await URLSession.shared.data(from: URL(string: "https://api.nasa.gov/planetary/apod?api_key=WBd6KIwrJohInTFEi1XSZA7ERws6opS3KLm2XhSH&start_date=\(lowerBoundString)&end_date=\(upperBoundString)")!)
             models += try JSONDecoder().decode([APOD].self, from: data)
+
+            upperDate = lowerDate
+            lowerDate = Calendar.current.date(byAdding: .day, value: -19, to: lowerDate)!
         } catch {
             print(error.localizedDescription)
         }
@@ -31,7 +47,18 @@ struct HomeView: View {
                 LazyVStack(spacing: 16) {
                     ForEach(viewModel.models, id: \.id) { apod in
                         APODCard(apod: apod)
+                            .onAppear {
+                                if apod.id == viewModel.models.last!.id {
+                                    Task {
+                                        await viewModel.getPhotos()
+                                    }
+                                }
+                            }
                     }
+
+                    ProgressView()
+                        .progressViewStyle(.circular)
+                        .frame(height: 120)
                 }
             }
             .task {
@@ -45,13 +72,14 @@ struct APODCard: View {
     let apod: APOD
 
     var body: some View {
-        VStack {
+        VStack(spacing: .zero) {
             // TODO: Image
             WebImage(url: URL(string: apod.url))
                 .onSuccess { pImage, data, _ in
 
                 }
                 .resizable()
+                .indicator(.activity(style: .large))
                 .scaledToFill()
                 .frame(height: 230)
                 .clipped()
@@ -65,9 +93,14 @@ struct APODCard: View {
                     .lineLimit(3)
             }
             .foregroundColor(Color.white)
+            .padding(8)
             .background(Color.black)
         }
         .clipShape(RoundedRectangle(cornerRadius: 8))
+        .overlay {
+            RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 5)
+        }
+        .padding(.horizontal, 12)
 
     }
 }
