6ea9a3018fec56255b89702440c9619a01cce41b
GokmenAkarPF <gokmen@propertyfinder.ae>
2023-09-05 16:01:04 +0300

    Initial Commit

---
+++ APOD/APODApp.swift
@@ -0,0 +1,17 @@
+//
+//  APODApp.swift
+//  APOD
+//
+//  Created by Gokmen Akar on 5.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct APODApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
